f9f7e350c143650a517816767ee4725abb0271ae
ycoodr <ycoodr@gmail.com>
2022-06-30 11:48:22 +0300

    модальное окно

--- frontend/src/app/department/department.component.ts
+++ frontend/src/app/department/department.component.ts
@@ -12,6 +12,9 @@ export class DepartmentComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   departments:any=[]
+  modalTitle = "";
+  DepartmentId = 0;
+  DepartmentName = "";
 
 
   ngOnInit(): void {
@@ -23,6 +26,56 @@ export class DepartmentComponent implements OnInit {
     .subscribe(data => {
       this.departments = data;
     });
+  }
+
+  addClick(){
+    this.modalTitle = "Add department";
+    this.DepartmentId = 0;
+    this.DepartmentName = "";
+  }
+
+  editClick(dep: any){
+    this.modalTitle = "Edit department";
+    this.DepartmentId = dep.DepartmentId;
+    this.DepartmentName = dep.DepartmentName;
+  }
+
+  createClick(){
+    var val = {
+      DepartmentName: this.DepartmentName
+    }
+
+    this.http.post(environment.API_URL + 'department', val)
+    .subscribe(res => {
+      alert(res.toString());
+      this.refreshList();
+    });
+  }
+
+  updateClick(){
+    var val = {
+      DepartmentId: this.DepartmentId,
+      DepartmentName: this.DepartmentName
+    }
+
+    this.http.put(environment.API_URL + 'department', val)
+    .subscribe(res => {
+      alert(res.toString());
+      this.refreshList();
+    });
+  }
+
+  deleteClick(id:any){
+    if(confirm('Are you sure?')){
+
+      this.http.delete(environment.API_URL + 'department/' + id)
+      .subscribe(res => {
+        alert(res.toString());
+        this.refreshList();
+      });
+    }
+
+   
   }
 
 }
