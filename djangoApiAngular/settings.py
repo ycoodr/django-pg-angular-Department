703bcebf3d4ac384b2796dc4dafc16fd42ef6e99
ycoodr <ycoodr@gmail.com>
2022-06-30 09:43:47 +0300

    отправка фото и конец бэкэнда

--- djangoApiAngular/settings.py
+++ djangoApiAngular/settings.py
@@ -11,7 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
+
+BASE_DIR = Path(__file__).resolve(strict = True).parent.parent
+MEDIA_URL = '/Photos/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "Photos")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
---
+++ pg.sql
@@ -0,0 +1,7 @@
+﻿select * from public."EmployeeApp_departments";
+insert into public."EmployeeApp_departments" values (5, 'IT');
+insert into public."EmployeeApp_departments" values (8, 'Support'); 
+
+select * from public."EmployeeApp_employees"
+
+insert into public."EmployeeApp_employees" values (1,  'IT', '2021-12-12', 'Bob','anonymous.png');
