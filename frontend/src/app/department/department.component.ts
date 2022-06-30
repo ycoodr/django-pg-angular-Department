d058cee44c15634382212d4057fb16be6d27681b
ycoodr <ycoodr@gmail.com>
2022-06-30 14:08:44 +0300

    angular сортировка

--- frontend/src/app/department/department.component.ts
+++ frontend/src/app/department/department.component.ts
@@ -11,10 +11,14 @@ export class DepartmentComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
 
-  departments:any=[]
+  departments:any=[];
   modalTitle = "";
   DepartmentId = 0;
   DepartmentName = "";
+
+  DepartmentIdFilter = "";
+  DepartmentNameFilter = "";
+  departmentsWithoutFilter: any=[];
 
 
   ngOnInit(): void {
@@ -25,6 +29,7 @@ export class DepartmentComponent implements OnInit {
     this.http.get<any>(environment.API_URL + 'department')
     .subscribe(data => {
       this.departments = data;
+      this.departmentsWithoutFilter = data;
     });
   }
 
@@ -73,9 +78,31 @@ export class DepartmentComponent implements OnInit {
         alert(res.toString());
         this.refreshList();
       });
+    }}
+
+    FilterFn(){
+      var DepartmentIdFilter = this.DepartmentIdFilter;
+      var DepartmentNameFilter = this.DepartmentNameFilter;
+
+      this.departments = this.departmentsWithoutFilter.filter(
+        function(el:any)
+        {
+          return el.DepartmentId.toString().toLowerCase().includes(DepartmentIdFilter.toString().trim().toLowerCase())
+           &&
+          el.DepartmentName.toString().toLowerCase().includes(DepartmentNameFilter.toString().trim().toLowerCase())
+          }
+      );
     }
 
-   
-  }
+    sortResult(prop:any, asc: any){
+      this.departments = this.departmentsWithoutFilter.sort(function(a:any, b:any){
+        if(asc){
+          return (a[prop] > b[prop])? 1 : ((a[prop] < b[prop])? -1:0);
+        }
+        else {
+          return (b[prop] > a[prop])? 1 : ((b[prop] < a[prop])? -1:0);
+        }
+      });
+    }
 
 }
